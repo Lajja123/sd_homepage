fe753c404f0c1214992c0b71027961b9fe9920ff
Lajja123 <lajja.lamprostech@gmail.com>
2024-05-24 17:15:51 +0530

    get started section completed

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,15 @@ import Navbar from "./navbar";
 import home from "./home.module.css";
 import "../app/chain.css";
 import a from "./assests/1.png";
+import {
+  faLink,
+  faMagnifyingGlassChart,
+  faRectangleList,
+  faShuffle,
+  faShare,
+  faUser,
+  faWallet,
+} from "@fortawesome/free-solid-svg-icons";
 import b from "./assests/2.png";
 import c from "./assests/3.png";
 import box1 from "./assests/box1.png";
@@ -10,6 +19,7 @@ import box2 from "./assests/box2.png";
 import box3 from "./assests/box3.png";
 import outer from "./assests/outer.png";
 import middle from "./assests/middle.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import inner from "./assests/inner.png";
 import iconLogo from "./assests/disperse.png";
 import base from "./assests/base.png";
@@ -296,44 +306,237 @@ export default function Home() {
         </div>
       </section>
       <section>
-      <div className={home.sec21}>
-     <div className={home.chainCircle}> 
-      <div className={home.outerWrapper}>
-        <Image src={outer} style={{width:"100%", height:"100%"}}alt='none'className={home.outercircle}/>
-     
-      </div>
-      <div className={home.logoWrapper}>
-      <div className={home.mainlogo}>
-      <div className={home.logodiv}>
-      <Image src={iconLogo} width={100} height={100} alt='none'/>
-        </div>
-        </div>
-      </div>
-      <div className={home.MidWrapper}>
-      <Image src={middle} alt='none' className={home.midcircle}/>
-      </div>
-      <div className={home.innerWrapper}>
-      <Image src={inner} alt='none' className={`${home.innercircle} `}/>
-      <Image src={base} alt='none' className={`${home.base} ${home.logoinner}`}/>
-      <Image src={eth} alt='none' className={`${home.eth} ${home.logoinner}`}/>
-      <Image src={optimism} alt='none' className={`${home.optimism} ${home.logoinner}`}/>
-      <Image src={mode} alt='none' className={`${home.mode} ${home.logoinner}`}/>
-      <Image src={scrolll} alt='none' className={`${home.scrolll} ${home.logoinner}` }/>
-      <Image src={tron} alt='none' className={`${home.tron} ${home.logoinner}` }/>
-      </div>
-    </div>
+        <div className={home.sec21}>
+          <div className={home.chainCircle}>
+            <div className={home.outerWrapper}>
+              <Image
+                src={outer}
+                style={{ width: "100%", height: "100%" }}
+                alt="none"
+                className={home.outercircle}
+              />
+            </div>
+            <div className={home.logoWrapper}>
+              <div className={home.mainlogo}>
+                <div className={home.logodiv}>
+                  <Image src={iconLogo} width={100} height={100} alt="none" />
+                </div>
+              </div>
+            </div>
+            <div className={home.MidWrapper}>
+              <Image src={middle} alt="none" className={home.midcircle} />
+            </div>
+            <div className={home.innerWrapper}>
+              <Image
+                src={inner}
+                alt="none"
+                className={`${home.innercircle} `}
+              />
+              <Image
+                src={base}
+                alt="none"
+                className={`${home.base} ${home.logoinner}`}
+              />
+              <Image
+                src={eth}
+                alt="none"
+                className={`${home.eth} ${home.logoinner}`}
+              />
+              <Image
+                src={optimism}
+                alt="none"
+                className={`${home.optimism} ${home.logoinner}`}
+              />
+              <Image
+                src={mode}
+                alt="none"
+                className={`${home.mode} ${home.logoinner}`}
+              />
+              <Image
+                src={scrolll}
+                alt="none"
+                className={`${home.scrolll} ${home.logoinner}`}
+              />
+              <Image
+                src={tron}
+                alt="none"
+                className={`${home.tron} ${home.logoinner}`}
+              />
+            </div>
+          </div>
 
-    <div className={home.flexContent}>
-      <h2>Securely connect every blockchain</h2>
-      <div className={home.flexpera}>
-      Synapse is comprised of a cross-chain messaging framework and an economically secure method to reach consensus on the validity of cross-chain transactions, enabling developers to build truly native cross-chain apps.
-      </div>
-      <div>
-        <button className={home.hApp}>App</button>
-      </div>
-    </div> 
-     
-    </div>
+          <div className={home.flexContent}>
+            <h2>Securely connect every blockchain</h2>
+            <div className={home.flexpera}>
+              Synapse is comprised of a cross-chain messaging framework and an
+              economically secure method to reach consensus on the validity of
+              cross-chain transactions, enabling developers to build truly
+              native cross-chain apps.
+            </div>
+            <div>
+              <button className={home.hApp}>App</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={home.sec3Main}>
+        <div className={home.snakeSection}>
+          <div className={home.divheadertag}>
+            <h3 className={home.chains}>SmartDisperse Walkthrough</h3>
+          </div>
+          <div className={home.container}>
+            <div className={`${home.stepWrapper} ${home.moveLine}`}>
+              <article
+                className={`${home.lineStep} ${home.lineStep1}`}
+                style={{ left: "56%", top: "-2%" }}
+              >
+                <span className={home.num}>
+                  {" "}
+                  <FontAwesomeIcon
+                    icon={faWallet}
+                    style={{ fontSize: "10px" }}
+                  />
+                </span>
+                <p className={home.bottom}>Connect Wallet</p>
+              </article>
+
+              <article
+                className={`${home.lineStep} ${home.lineStep2}`}
+                style={{ right: " -9.5%", top: "17%" }}
+              >
+                <span className={home.num}>
+                  {" "}
+                  <FontAwesomeIcon icon={faLink} />
+                </span>
+                <p className={home.left}>Select Chain</p>
+              </article>
+              <article
+                className={`${home.lineStep} ${home.lineStep3}`}
+                style={{ left: "68%", top: "44%" }}
+              >
+                <span className={home.num}>
+                  {" "}
+                  <FontAwesomeIcon icon={faShuffle} />
+                </span>
+                <p className={home.bottom}>
+                  Select Transactions Type
+                  <p>(same-chain/cross-chain ) </p>
+                </p>
+              </article>
+
+              <article
+                className={`${home.lineStep} ${home.lineStep4}`}
+                style={{ left: "32%", top: "44.5%" }}
+              >
+                <span className={home.num}>
+                  <FontAwesomeIcon icon={faRectangleList} />
+                </span>
+                <p className={home.top}>List your transactions</p>
+              </article>
+
+              <article
+                className={`${home.lineStep} ${home.lineStep5}`}
+                style={{ left: "-10.5%", top: "68%" }}
+              >
+                <span className={home.num}>
+                  {" "}
+                  <FontAwesomeIcon icon={faUser} />
+                </span>
+                <p className={home.right}>
+                  Verify the Recipient address & amount
+                </p>
+              </article>
+
+              <article
+                className={`${home.lineStep} ${home.lineStep6}`}
+                style={{ left: "21%", top: "93%" }}
+              >
+                <span className={home.num}>
+                  <FontAwesomeIcon icon={faShare} />
+                </span>
+                <p className={home.bottom}>Send Transactions</p>
+              </article>
+
+              <article
+                className={`${home.lineStep} ${home.lineStep7}`}
+                style={{ left: "53%", top: "93%" }}
+              >
+                <span className={home.num}>
+                  {" "}
+                  <FontAwesomeIcon icon={faMagnifyingGlassChart} />
+                </span>
+                <p className={home.top}>View History of Transactions</p>
+              </article>
+
+              <svg
+                width="100%"
+                viewBox="0 0 1156 608"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className={home.path}
+                  d="m560.30957,10.588011c0,0 438.0947,1.90476 439.04708,1.90476c0.95238,0 144.57857,-1.02912 143.80934,137.14269c-0.76923,138.17181 -116.81095,142.30859 -131.61967,143.8923c-14.80873,1.58372 -840.41472,-0.71429 -860.5941,0.71429c-20.17938,1.42858 -148.4991,6.80903 -146.83244,147.05973c1.66666,140.2507 129.52365,152.14266 129.33243,151.27321c0.19122,0.86945 815.268425,2.687632 951.42748,0"
+                  opacity="0.5"
+                  strokeWidth="2"
+                  stroke="#2567d1"
+                  stroke-dasharray="10 10"
+                  fill="none"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={home.sec3Main}>
+        <div className={home.sec5Title}>Get started now</div>
+        <div className={home.outerdivofusecases}>
+          <div className={home.cardcontainer}>
+            <div className={home.cardGet}>
+              <div className={home.cardofgetstart}>
+                <div className={home.cardcontent}>
+                  <div className={home.cardcontent1}>Cross-chain</div>
+                  <div className={home.cardcontent2}>
+                    Unlocking Cross-Chain Transactions: Seamlessly Connect
+                    Blockchain
+                  </div>
+                  <div className={home.buttongetstart}>
+                    <div>
+                      <button className={home.getstartbtn}>Start now ➔</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={home.cardofgetstart}>
+                <div className={home.cardcontent}>
+                  <div className={home.cardcontent1}>Same-chain</div>
+                  <div className={home.cardcontent2}>
+                    Streamlining Same-Chain Transactions: Enhancing Efficiency
+                    and Accessibility
+                  </div>
+                  <div className={home.buttongetstart}>
+                    <div>
+                      <button className={home.getstartbtn}>Start now ➔</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={home.cardofgetstart}>
+                <div className={home.cardcontent}>
+                  <div className={home.cardcontent1}>Documentation</div>
+                  <div className={home.cardcontent2}>
+                    Unraveling SmartDisperse: A Comprehensive Guide to Seamless
+                    Cross-Chain Communication
+                  </div>
+                  <div className={home.buttongetstart}>
+                    <div>
+                      <button className={home.getstartbtn}>Start now ➔</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
